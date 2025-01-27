1c03d0c797c3efaf2bd399e02d020a3bcc947076
Tom <tom.cohen1996@gmail.com>
2025-01-27 04:33:07 +0200

    README, Procfile

--- main.py
+++ main.py
@@ -125,4 +125,4 @@ async def research(request: Request):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", 5000)))
+    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", 8000)))
